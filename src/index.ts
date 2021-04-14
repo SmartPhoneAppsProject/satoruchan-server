64535d61d49be0dd16ee3922c956760adbad1ca3
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2021-04-14 10:49:22 +0900

    add:postgres package

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,35 @@
 import express from 'express'
+import { Pool } from 'pg'
 
 const main = () => {
   const app = express()
 
-  app.use(express.json())
-  app.use(express.urlencoded({extended: true}))
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    // ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : null
+  })
 
-  app.get("/", (_,res) => {
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+
+  app.get("/", (_, res) => {
     res.send("hogehoge")
   })
 
-  app.post("/update", (req,res) => {
+  app.get("/db", async (_, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM member_list')
+      const results = { 'results': (result) ? result.rows : null }
+      res.send(results)
+      client.release()
+    } catch (err) {
+      console.log(err)
+      res.send("ERR: " + err)
+    }
+  })
+
+  app.post("/update", (req, res) => {
     const data = req.body
     console.log(data)
     res.status(200)
