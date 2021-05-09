efb727197eb5e78995912430e5ad6d4588f18762
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-05-09 19:56:05 +0900

    datebaseの初期値を変更

--- container/db/docker-entrypoint-initdb.d/init.sh
+++ container/db/docker-entrypoint-initdb.d/init.sh
@@ -5,8 +5,11 @@ psql -U postgres satoruchan<<- EOSQL
 CREATE TABLE member_list (name text, macaddress text);
 CREATE TABLE active_member (macaddress text);
 
-INSERT INTO member_list values ('ギア','FF:FF:FF:FF:FF:F1');
-INSERT INTO member_list values ('スタヌ','FF:FF:FF:FF:FF:F2');
+INSERT INTO member_list values ('TokyoTanaka','FF:FF:FF:FF:FF:F1');
+INSERT INTO member_list values ('JankenJony','FF:FF:FF:FF:FF:F2');
+INSERT INTO member_list values ('KamikazeBoy','FF:FF:FF:FF:FF:F3');
+INSERT INTO member_list values ('SpairLib','FF:FF:FF:FF:FF:F4');
+INSERT INTO member_list values ('DJ-Santamonica','FF:FF:FF:FF:FF:F5');
 
 
 INSERT INTO active_member values ('FF:FF:FF:FF:FF:F2');
