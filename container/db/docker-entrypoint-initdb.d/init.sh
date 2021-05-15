1df9bfebec899025e918a1621fd74253ed6229ec
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-05-15 16:36:17 +0900

    database fixing

--- container/db/docker-entrypoint-initdb.d/init.sh
+++ container/db/docker-entrypoint-initdb.d/init.sh
@@ -5,11 +5,11 @@ psql -U postgres satoruchan<<- EOSQL
 CREATE TABLE member_list (name text, macaddress text);
 CREATE TABLE active_member (macaddress text);
 
-INSERT INTO member_list values ('TokyoTanaka','FF:FF:FF:FF:FF:F1');
-INSERT INTO member_list values ('JankenJony','FF:FF:FF:FF:FF:F2');
-INSERT INTO member_list values ('KamikazeBoy','FF:FF:FF:FF:FF:F3');
-INSERT INTO member_list values ('SpairLib','FF:FF:FF:FF:FF:F4');
-INSERT INTO member_list values ('DJ-Santamonica','FF:FF:FF:FF:FF:F5');
+INSERT INTO member_list values ('Tokyo Tanaka','FF:FF:FF:FF:FF:F1');
+INSERT INTO member_list values ('Jean-Ken Jony','FF:FF:FF:FF:FF:F2');
+INSERT INTO member_list values ('Kamikaze Boy','FF:FF:FF:FF:FF:F3');
+INSERT INTO member_list values ('Spear Rib','FF:FF:FF:FF:FF:F4');
+INSERT INTO member_list values ('DJ Santa Monica','FF:FF:FF:FF:FF:F5');
 
 
 INSERT INTO active_member values ('FF:FF:FF:FF:FF:F2');
